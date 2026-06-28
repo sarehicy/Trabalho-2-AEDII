0cdf4d1ff0ef7e5ecc2066e18c7204289cef1053
Meicy <sarahmeicymb@gmail.com>
2026-06-28 16:11:56 -0300

    Função para inserção ordenada de vértices no grafo

--- grafoEstacoes.c
+++ grafoEstacoes.c
@@ -8,7 +8,7 @@ grafoEstacoes *criarGrafoEstacoes(){
     grafoEstacoes *grafoEst = malloc(sizeof(grafoEstacoes));
     // verificar grafoEstacoes
 
-    grafoEst->vertices = malloc(sizeof(verticeEstacoes*));
+    grafoEst->vertices = malloc(sizeof(verticeEstacoes*));  // vetor de vertices
     grafoEst->qtdVertices = 0;
 
     return grafoEst;
@@ -24,16 +24,42 @@ void criarVertice(grafoEstacoes *grafo, char *nomeEstacao, int tamNomeEstacao){
 
     vertice->arestas = NULL;
 
-    // inserindo vertice no grafo
-    int pos = grafo->qtdVertices;
-    grafo->vertices[pos] = vertice;
-    pos+=1;
-    grafo->qtdVertices = pos;
+    // inserindo vertice no grafo respeitando sua ordenação
+    insertionSort(grafo, vertice);
 
+    int newQtd = grafo->qtdVertices +1;
+    grafo->qtdVertices = newQtd;
 
     // Ajustando novo tamanho do vetor de vertices do grafo
-    verticeEstacoes **aux = realloc(grafo->vertices, sizeof(verticeEstacoes*)*(pos+1));
+    verticeEstacoes **aux = realloc(grafo->vertices, sizeof(verticeEstacoes*)*(newQtd+1));
     if (!aux) exit(0);
     grafo->vertices = aux;
+}
+
+void insertionSortVertice(grafoEstacoes *grafo, verticeEstacoes *vertice){
+
+    // Insere vertice no final do vetor
+    int qtd = grafo->qtdVertices;
+    grafo->vertices[qtd] = vertice;
+
+    // Insertion sort para inseri-lo na posição correta
+    verticeEstacoes **vertices = grafo->vertices;
+    int comp; verticeEstacoes *aux;
+
+    int i = (qtd-1);
+    while (i>=0){
+        
+        aux = vertices[i];
+        comp = strcmp(vertice->nomeEstacao, aux->nomeEstacao);
+
+        // vertice está na posição correta
+        if (comp > 0) break;
+
+        vertices[i+1] = vertices[i];
+        vertices[i] = vertice;
+
+        i-=1;
+    }
+
 }
 
--- grafoEstacoes.h
+++ grafoEstacoes.h
@@ -3,12 +3,20 @@
 
 #include "structs.h"
 
+void verificarGrafo(grafoEstacoes *grafo);
+
 // Cria um grafo 
 grafoEstacoes *criarGrafoEstacoes();
 
 // Função que cria um vértice e insere no grafo
 void criarVertice(grafoEstacoes *grafo, char *nomeEstacao, int tamNomeEstacao);
 
+// Insere um vértice no grafo ordenando-o
+void insertionSortVertice(grafoEstacoes *grafo, verticeEstacoes *vertice);
+
+void criarAresta();
+
+void insertionSortAresta();
 
 
 #endif
